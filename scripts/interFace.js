28bf03700b17f426458445b7bbe02252a4e4eae9
Thiago Porfirio <thiagovporfirio@gmail.com>
2022-06-15 15:19:44 -0300

    Todos arquivos

---
+++ scripts/interFace.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+
+    })
+
+
+})
+
+
+function handleClick(event){
+
+    let square = event.target;
+    let postion = square.id;
+
+    if(handleMove(postion)) {
+
+            setTimeout(() => {
+                alert(" O Jogo Acabou, O vencedor da vez foi: " + playerTime )
+            }, 10)
+            
+    }
+    
+    updateSquares()
+}
+
+function updateSquares(){
+
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach((square) => {
+        let postion = square.id;
+        let symbol = board[postion];
+
+        if(symbol != ''){
+            square.innerHTML = `<div class='${symbol}'></div>`
+
+        }
+    })
+}
